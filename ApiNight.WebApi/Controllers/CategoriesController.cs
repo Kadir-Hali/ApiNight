278cce3e85de5f3f1e01ae8e0a159526bb5372b9
Kadir-Hali <kadirhali07@gmail.com>
2025-07-04 22:16:36 +0300

    04.07.25

--- ApiNight.WebApi/Controllers/CategoriesController.cs
+++ ApiNight.WebApi/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiNight.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiNight.WebApi.Controllers
@@ -7,5 +8,17 @@ namespace ApiNight.WebApi.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return Ok(values);
+        }
     }
 }
